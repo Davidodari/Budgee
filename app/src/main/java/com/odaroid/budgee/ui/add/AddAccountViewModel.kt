9cee42307e96b4e9275b616ae82ae5ddd2649adf
Davidodari <davidkibzodari@gmail.com>
2019-06-12 10:11:02 +0300

    [Feature #166494396] Button State Logic with viewmodel

--- app/src/main/java/com/odaroid/budgee/ui/add/AddAccountViewModel.kt
+++ app/src/main/java/com/odaroid/budgee/ui/add/AddAccountViewModel.kt
@@ -1,5 +1,30 @@
 package com.odaroid.budgee.ui.add
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.odaroid.budgee.data.accounts.Account
+import timber.log.Timber
 
-class AddAccountViewModel : ViewModel()
+class AddAccountViewModel : ViewModel() {
+
+    private val _isReadyToSave: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+    val isReadyToSave: LiveData<Boolean>
+        get() = _isReadyToSave
+
+    init {
+        _isReadyToSave.value = false
+    }
+
+    fun enableSaveButton() {
+        _isReadyToSave.value = true
+    }
+
+    fun disableSaveButton() {
+        _isReadyToSave.value = false
+    }
+
+    fun saveAccountInfo(account: Account) {
+        Timber.d("New Acc: $account")
+    }
+}
